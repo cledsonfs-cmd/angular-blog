0ceaa8c2f43ba91c805f3818d55288e965690a5c
Cledson Francisco Silva <70558441+cledsonfs-cmd@users.noreply.github.com>
2024-09-13 14:00:40 -0300

    fix:update

---
+++ src/app/data/dataFAke.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Lorem Ipsum is simply dummy text of the printing and typesetting',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    photoCover:
+      'https://centraltatooine.com.br/wp-content/uploads/2023/12/49.jpg',
+  },
+  {
+    id: '2',
+    title: 'Lorem Ipsum is simply dummy text of the printing and typesetting',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    photoCover:
+      'https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/04/Every-Star-Wars-Movie-And-TV-Show-In-Chronological-Order.jpg',
+  },
+  {
+    id: '3',
+    title: 'Lorem Ipsum is simply dummy text of the printing and typesetting',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    photoCover:
+      'https://www.denofgeek.com/wp-content/uploads/2023/10/Star-Wars-Live-Action-Series-Ranked.jpg?fit=1200%2C675',
+  },
+  {
+    id: '4',
+    title: 'Lorem Ipsum is simply dummy text of the printing and typesetting',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    photoCover:
+      'https://discussingfilm.net/wp-content/uploads/2023/05/Star-Wars-TV-Shows-Ranked.jpg',
+  },
+];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { dataFake } from '../../data/dataFAke';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  photoCover: string = dataFake[0].photoCover;
+  title: string = dataFake[0].title;
+  description: string = dataFake[0].description;
 
+  data: any[] = dataFake;
 }
